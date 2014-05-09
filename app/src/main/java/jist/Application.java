9a9b834394d1269564c8f05e698c4bd116fdc972
Nikhil Kothari <nikhilk@live.com>
2014-05-08 23:07:11 -0700

    Added core module as dependency of app module

--- app/src/main/java/jist/Application.java
+++ app/src/main/java/jist/Application.java
@@ -1,11 +1,14 @@
 // Application.java
+// jist/app
 //
 
 package jist;
 
+import jist.core.*;
+
 public final class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(Settings.GREETING);
     }
 }
---
+++ core/src/main/java/jist/core/Settings.java
@@ -0,0 +1,11 @@
+// Settings.java
+// jist/core
+//
+
+package jist.core;
+
+public final class Settings {
+
+    public final static String GREETING = "Hello World!";
+}
+
